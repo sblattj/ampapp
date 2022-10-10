73a2f59c14ecae94457fbca2fbce0f08429a4842
Stephen Blatt <sblatt@centricsoftware.com>
2022-10-10 16:32:17 -0700

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+export default function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Let's change the look of this page.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+	  <Authenticator>
+		{({ signOut, user }) => (
+		  <main>
+			<h1>Hello {user!.username}</h1>
+			<button onClick={signOut}>Sign out</button>
+		  </main>
+		)}
+	  </Authenticator>
 	);
-}
-
-export default App;
+  }
