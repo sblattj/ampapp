3e79ce8f302cd15f75926731233b41522ae44fb9
Stephen Blatt <sblatt@centricsoftware.com>
2022-10-10 22:12:49 -0700

    auth remove email

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,27 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "ampapp3c6199a8": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
+        "ampappauth": {
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string",
             "CreatedSNSRole": "string"
+        }
+    },
+    "function": {
+        "AdminQueriesd8157567": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "AdminQueries": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
         }
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, CheckboxField, useAuthenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
+
+// const formFields = {
+// 	signUp: {
+// 	  confirmation_code: {
+// 		labelHidden: false,
+// 		label: 'New Label',
+// 		placeholder: 'Enter your Confirmation Code:',
+// 		isRequired: false,
+// 	  },
+// 	},
+//   };
 
 export default function App() {
 	return (
-	  <Authenticator>
-		{({ signOut, user }) => (
-		  <main>
-			<h1>Hello {user!.username}</h1>
-			<button onClick={signOut}>Sign out</button>
-		  </main>
-		)}
-	  </Authenticator>
+		<Authenticator
+			// Default to Sign Up screen
+			initialState="signUp"
+			// formFields={}
+		>
+			{({ signOut, user }) => (
+				<main>
+					<h1>Hello {user!.username}</h1>
+					<button onClick={signOut}>Sign out</button>
+				</main>
+			)}
+		</Authenticator>
 	);
-  }
+}
